7c8dda4f133cc54fdbe28e40ffd8c7e7598edb2f
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-08-28 23:28:00 +0430

    using "useState" __first time use Hock

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './components/product/Product'
 
-class App extends React.Component {
-    state = {
-        products: [{ title: 'book1', price: '90' }, { title: 'book2', price: '80' }, { title: 'book3', price: '70' }]
-    }
+// class App extends React.Component {
+//     state = {
+//         products: [{ title: 'book1', price: '90' }, { title: 'book2', price: '80' }, { title: 'book3', price: '70' }]
+//     }
 
-    changePriceHandler = () => {
+//     changePriceHandler = () => {
+//         let newState = {
+//             products: [{ title: 'book1', price: '10' }, { title: 'book2', price: '10' }, { title: 'book3', price: '10' }]
+//         }
+//         this.setState(newState)
+//     }
+//     render() {
+//         return (
+//             <div id='main' className='container'>
+//                 Hello World !!!!!
+//                 <Product title={this.state.products[0].title} price={this.state.products[0].price} />
+//                 <Product title={this.state.products[1].title} price={this.state.products[1].price} />
+//                 <Product title={this.state.products[2].title} price={this.state.products[2].price} />
+//                 <button onClick={this.changePriceHandler}>change price</button>
+//             </div>)
+//     }
+// }
+
+const App = () => {
+    let [Pstate, setPState] = useState({
+        products: [{ title: 'book1', price: '90' },
+        { title: 'book2', price: '80' },
+        { title: 'book3', price: '70' }]
+    });
+
+    let changePriceHandler = () => {
         let newState = {
-            products: [{ title: 'book1', price: '10' }, { title: 'book2', price: '10' }, { title: 'book3', price: '10' }]
+            products: [{ title: 'book1', price: '10' },
+            { title: 'book2', price: '10' },
+            { title: 'book3', price: '10' }]
         }
-        this.setState(newState)
+        setPState(newState)
+
     }
-    render() {
-        return (
-            <div id='main' className='container'>
-                Hello World !!!!!
-                <Product title={this.state.products[0].title} price={this.state.products[0].price} />
-                <Product title={this.state.products[1].title} price={this.state.products[1].price} />
-                <Product title={this.state.products[2].title} price={this.state.products[2].price} />
-                <button onClick={this.changePriceHandler}>change price</button>
-            </div>)
-    }
+    return (
+        <div id='main' className='container'>
+            Hello World !!!!!
+            <Product title={Pstate.products[0].title} price={Pstate.products[0].price} />
+            <Product title={Pstate.products[1].title} price={Pstate.products[1].price} />
+            <Product title={Pstate.products[2].title} price={Pstate.products[2].price} />
+            <button onClick={changePriceHandler}>change price</button>
+        </div>)
 }
 export default App
