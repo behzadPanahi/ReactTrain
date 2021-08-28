8defc1fc93068103c2a113d9e6a52ced3cbb6b1a
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-08-28 19:22:02 +0430

    first react code : make a element and render that

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,1 +1,7 @@
-
+import React from 'react'
+import ReactDom from 'react-dom'
+let element=React.createElement(
+  'div',
+  { id: 'main', className: 'container' },
+  'Hello World!!!')
+  ReactDom.render(element,document.getElementById('root'))
