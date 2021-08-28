211d5516a0ec1247fb36acd9c947fd661d3360d6
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-08-28 22:31:20 +0430

    event handeling and  change state

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,8 +2,15 @@ import React from 'react'
 import Product from './components/product/Product'
 
 class App extends React.Component {
-    state={
-        products:[{title:'book1' , price:'90'},{title:'book2' , price:'80'},{title:'book3' , price:'70'}]
+    state = {
+        products: [{ title: 'book1', price: '90' }, { title: 'book2', price: '80' }, { title: 'book3', price: '70' }]
+    }
+
+    changePriceHandler = () => {
+        let newState = {
+            products: [{ title: 'book1', price: '10' }, { title: 'book2', price: '10' }, { title: 'book3', price: '10' }]
+        }
+        this.setState(newState)
     }
     render() {
         return (
@@ -12,6 +19,7 @@ class App extends React.Component {
                 <Product title={this.state.products[0].title} price={this.state.products[0].price} />
                 <Product title={this.state.products[1].title} price={this.state.products[1].price} />
                 <Product title={this.state.products[2].title} price={this.state.products[2].price} />
+                <button onClick={this.changePriceHandler}>change price</button>
             </div>)
     }
 }
