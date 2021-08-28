84d463b2de9a3bd041622042a4212672007eddc9
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-08-28 22:21:11 +0430

    understanding state

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,15 +2,16 @@ import React from 'react'
 import Product from './components/product/Product'
 
 class App extends React.Component {
+    state={
+        products:[{title:'book1' , price:'90'},{title:'book2' , price:'80'},{title:'book3' , price:'70'}]
+    }
     render() {
         return (
             <div id='main' className='container'>
                 Hello World !!!!!
-                <Product title='book1' price='90' />
-                <Product title='book2' price='70'>
-                    discount : 20
-                </Product>
-                <Product title='book1' price='80' />
+                <Product title={this.state.products[0].title} price={this.state.products[0].price} />
+                <Product title={this.state.products[1].title} price={this.state.products[1].price} />
+                <Product title={this.state.products[2].title} price={this.state.products[2].price} />
             </div>)
     }
 }
