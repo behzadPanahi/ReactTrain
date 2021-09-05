dd3f8697ec33fe7a0114c4a37dd8fd5c7a1f3954
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-05 13:55:01 +0430

    rendering-content-conditionally

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,55 +4,54 @@ import './App.css'
 
 class App extends React.Component {
     state = {
-        products: [{ title: 'book1', price: '90' }, { title: 'book2', price: '80' }, { title: 'book3', price: '70' }]
-    }
-
-    changePriceHandler = (newTitle) => {
-        let newState = {
-            products: [
-                { title: newTitle, price: '10' }, 
-                { title: 'book2', price: '10' },
-                 { title: 'book3', price: '10' }]
-        }
-        this.setState(newState)
+        products: [{ title: 'book1', price: '90' },
+        { title: 'book2', price: '80' },
+        { title: 'book3', price: '70' }],
+        showProducts: false
     }
 
     changeInputHandler = (event) => {
         let newState = {
             products: [
-                { title: 'book1', price: '10' }, 
+                { title: 'book1', price: '10' },
                 { title: event.target.value, price: '10' },
-                 { title: 'book3', price: '10' }]
+                { title: 'book3', price: '10' }]
         }
         this.setState(newState)
     }
+
+    toggleHandler = () => {
+        const show = this.state.showProducts
+        this.setState({ showProducts: !show })
+    }
+
+
     render() {
-        const btn={
-            backgroundColor:"#7b1fa2",
-            color:"#ffffff",
-            font:"inherit",
-            border:"none",
-            outline:"none",
-            borderRadius:"3px",
-            padding:"0.6rem",
-            margin:"0.6rem ,auto"
-        }
         return (
             <div className='center'>
-                Book store
-                <Product title={this.state.products[0].title}
-                    price={this.state.products[0].price} />
-                <Product title={this.state.products[1].title}
-                    price={this.state.products[1].price}
-                    change={this.changeInputHandler} />
-                <Product title={this.state.products[2].title}
-                    price={this.state.products[2].price}
-                    click={this.changePriceHandler.bind(this,'new title')} />
-                <button 
-                onClick={()=>this.changePriceHandler('title bbb')}
-                style={btn}>
-                    change price</button>
-            </div>)
+                <h1>
+                    Book store
+                </h1>
+                <button
+                    onClick={this.toggleHandler}
+                    className='btn' >
+                    show/hidden
+                </button>
+                {
+                    this.state.showProducts ?
+                     <div>
+                     <Product title={this.state.products[0].title}
+                         price={this.state.products[0].price} />
+                     <Product title={this.state.products[1].title}
+                         price={this.state.products[1].price} 
+                         change={this.changeInputHandler}/>
+                     <Product title={this.state.products[2].title}
+                         price={this.state.products[2].price} />
+                 </div>
+                 :null
+                }  
+            </div>
+            )
     }
 }
 export default App
