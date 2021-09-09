8f26e06a4680065639b7a196f3a40bb1e9e6b36b
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-10 03:10:51 +0430

    key-in-lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,9 +4,10 @@ import './App.css'
 
 class App extends React.Component {
     state = {
-        products: [{ title: 'book1', price: '90' },
-        { title: 'book2', price: '80' },
-        { title: 'book3', price: '70' }],
+        products: [
+            { id: 1, title: 'book1', price: '90' },
+            { id: 2, title: 'book2', price: '80' },
+            { id: 3, title: 'book3', price: '70' }],
         showProducts: false
     }
 
@@ -15,25 +16,26 @@ class App extends React.Component {
         this.setState({ showProducts: !show })
     }
 
-    deleteProductHandler=(index)=>{
-        const tempProducts=[...this.state.products]
-        tempProducts.splice(index,1);
-        this.setState({priducts:tempProducts})
+    deleteProductHandler = (index) => {
+        const tempProducts = this.state.products
+        tempProducts.splice(index, 1);
+        this.setState({ priducts: tempProducts })
     }
     render() {
-        let products=null;
-        if(this.state.showProducts){
-            products= <div>
+        let products = null;
+        if (this.state.showProducts) {
+            products = <div>
                 {
-                    this.state.products.map((item ,index)=>{
-                        return <Product 
-                        title={item.title} 
-                        price={item.price}
-                        click={()=>this.deleteProductHandler(index)}
+                    this.state.products.map((item, index) => {
+                        return <Product
+                            key={item.id}
+                            title={item.title}
+                            price={item.price}
+                            click={() => this.deleteProductHandler(index)}
                         />
                     })
                 }
-        </div>
+            </div>
         }
         return (
             <div className='center'>
@@ -47,7 +49,7 @@ class App extends React.Component {
                 </button>
                 {products}
             </div>
-            )
+        )
     }
 }
 export default App
