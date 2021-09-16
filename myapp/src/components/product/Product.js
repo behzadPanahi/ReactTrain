7a00ad6049945f009b27321ec06dbae9f8b9572c
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-17 04:23:08 +0430

    using-react-fragment

--- myapp/src/components/product/Product.js
+++ myapp/src/components/product/Product.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './Product.css'
 import Container from '../../hoc/Container'
 
-class Product extends React.Component{
-   
-    render(){
-        return(
-           <Container>
+class Product extends React.Component {
+
+    render() {
+        return (
+            <React.Fragment>
                 <p key='1' onClick={this.props.click}>book title : {this.props.title}</p>
                 <p key='2'>book price : {this.props.price}</p>
                 <p key='3'> {this.props.children} </p>
-                <input key='4' type='text' onChange={this.props.change}/>
-                </Container> 
+                <input key='4' type='text' onChange={this.props.change} />
+            </React.Fragment>
         )
     }
 }
