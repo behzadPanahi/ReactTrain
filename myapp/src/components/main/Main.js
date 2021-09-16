9640fee9ff3249a3b38c4e80892ad21544bdab21
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-17 03:42:37 +0430

    useeffect-in-functional-components

--- myapp/src/components/main/Main.js
+++ myapp/src/components/main/Main.js
@@ -1,40 +1,57 @@
-import React from "react";
+import React ,{useEffect}from "react";
 import './Main.css'
-
-class Main extends React.Component{
-    constructor(props){
-        super(props)
-        console.log('Main.js constractor');
-    }
-    componentDidMount(){
-        console.log('Main.js componentDidMount');
-    }
-
-    shouldComponentUpdate(nextProps,nextState){
-        console.log('Main.js shouldComponentUpdate');
-        return true
-    }
-
-    getSnapshotBeforeUpdate(prevProps,prevState){
-        console.log('Main.js getSnapshotBeforeUpdate');
-        return null
-    }
-
-    componentDidUpdate(){
-        console.log('Main.js componentDidUpdate');
-    }
-render(){
-    console.log('Main.js rendered');
+ 
+const Main=(props)=>{
+useEffect(()=>{
+    console.log('Main.js Use Effect');
+})
     return(<div>
     <h1>
     Book store
 </h1>
 <button
-    onClick={this.props.click}
+    onClick={props.click}
     className='btn' >
     show/hidden
 </button>
     </div>)
+
+
 }
-}
+// class Main extends React.Component{
+//     constructor(props){
+//         super(props)
+//         console.log('Main.js constractor');
+//     }
+//     componentDidMount(){
+//         console.log('Main.js componentDidMount');
+//     }
+
+//     shouldComponentUpdate(nextProps,nextState){
+//         console.log('Main.js shouldComponentUpdate');
+//         return true
+//     }
+
+//     getSnapshotBeforeUpdate(prevProps,prevState){
+//         console.log('Main.js getSnapshotBeforeUpdate');
+//         return null
+//     }
+
+//     componentDidUpdate(){
+//         console.log('Main.js componentDidUpdate');
+//     }
+// render(){
+//     console.log('Main.js rendered');
+//     return(<div>
+//     <h1>
+//     Book store
+// </h1>
+// <button
+//     onClick={this.props.click}
+//     className='btn' >
+//     show/hidden
+// </button>
+//     </div>)
+// }
+// }
 export default Main
