1f0486d974f7f7da037048eb81dadb2e70b92a10
behzadPanahi <89600886+behzadPanahi@users.noreply.github.com>
2021-09-17 16:01:56 +0430

    using-ref

--- myapp/src/components/product/Product.js
+++ myapp/src/components/product/Product.js
@@ -3,14 +3,22 @@ import './Product.css'
 import Wrapper from '../../hoc/Wrapper'
 
 class Product extends React.Component {
-
+constructor(props){
+    super(props)
+    this.refElement=React.createRef()
+}
+componentDidMount(){
+this.refElement.current.focus()
+}
     render() {
         return (
             <React.Fragment>
                 <p key='1' onClick={this.props.click}>book title : {this.props.title}</p>
                 <p key='2'>book price : {this.props.price}</p>
                 <p key='3'> {this.props.children} </p>
-                <input key='4' type='text' onChange={this.props.change} />
+                <input
+                ref={this.refElement}
+                 key='4' type='text' onChange={this.props.change} />
             </React.Fragment>
         )
     }
